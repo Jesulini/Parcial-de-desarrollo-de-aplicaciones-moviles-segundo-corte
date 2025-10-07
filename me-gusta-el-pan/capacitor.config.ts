6559395bf825021377eef1b14a1422383eb4e2f8
Jesulini <jesulini14@gmail.com>
2025-10-07 09:28:03 -0500

    iniciando el infierno

---
+++ me-gusta-el-pan/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'me gusta el pan',
+  webDir: 'www'
+};
+
+export default config;
